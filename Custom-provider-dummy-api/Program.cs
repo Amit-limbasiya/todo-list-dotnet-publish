61e1eaa90cef27b4d95fe54661bd594f59d6482a
Amit-limbasiya <amit.limbasiya@marutitech.com>
2024-06-10 15:12:52 +0530

    Todo list done

---
+++ Custom-provider-dummy-api/Program.cs
@@ -0,0 +1,65 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+List<string> todoList = new();
+
+app.MapGet("/get", () =>
+{
+    Console.WriteLine("get endpoint hit");
+    return todoList;
+})
+.WithName("GetToDoList")
+.WithOpenApi();
+
+
+app.MapPost("/create", (List<string> things) =>
+{
+    todoList = new List<string>(things);
+    //{
+    //    "extra"
+    //};
+    Console.WriteLine("create endpoint hit");
+    return todoList;
+})
+.WithName("MakeToDoList")
+.WithOpenApi();
+
+app.MapPut("/update", (List<string> things) =>
+{
+    todoList = new List<string>(things.Select(x => x.ToString()));
+    Console.WriteLine("update endpoint hit");
+    return todoList;
+})
+.WithName("UpdateToDoList")
+.WithOpenApi();
+
+app.MapDelete("/delete", () =>
+{
+    Console.WriteLine("delete endpoint hit");
+    todoList = new List<string>();
+    return todoList;
+})
+.WithName("DeleteToDoList")
+.WithOpenApi();
+
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
